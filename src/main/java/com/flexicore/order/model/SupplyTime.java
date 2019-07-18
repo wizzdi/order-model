740221e49deece83979c80bf4ccf0c2a0d0a1837
asaf <asaf@flexi-core.com>
2019-07-18 14:35:36 +0300

    1.0.4 added supply time

---
+++ src/main/java/com/flexicore/order/model/SupplyTime.java
@@ -0,0 +1,56 @@
+package com.flexicore.order.model;
+
+import com.flexicore.model.Baseclass;
+
+import javax.persistence.Entity;
+
+@Entity
+public class SupplyTime extends Baseclass {
+    static SupplyTime s_Singleton = new SupplyTime();
+    public static SupplyTime s() {
+        return s_Singleton;
+    }
+
+
+    private int dayOfTheWeek;
+    private int hour;
+    private int minute;
+    private int second;
+
+
+    public int getDayOfTheWeek() {
+        return dayOfTheWeek;
+    }
+
+    public <T extends SupplyTime> T setDayOfTheWeek(int dayOfTheWeek) {
+        this.dayOfTheWeek = dayOfTheWeek;
+        return (T) this;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public <T extends SupplyTime> T setHour(int hour) {
+        this.hour = hour;
+        return (T) this;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public <T extends SupplyTime> T setMinute(int minute) {
+        this.minute = minute;
+        return (T) this;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public <T extends SupplyTime> T setSecond(int second) {
+        this.second = second;
+        return (T) this;
+    }
+}
